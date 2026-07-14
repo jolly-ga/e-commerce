0b6b2f6c9b8bf7aad483839c6e9dc214c22afaa4
Ana Garcia <jolly20222368@gmail.com>
2026-07-13 21:55:03 -0300

    feat(produto.ts): adiciona função lista de produtos

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import{ UpperCasePipe}from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+//cria a classe Produto com variáveis (preco, produto) e condicionais (mostrarProduto, mostrarPreco)
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000; //! preco = preço
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos =[
+    {nome:'Teclado', preco:49.99},
+    {nome:'Mouse', preco: 29.99},
+    {nome:'Monitor', preco: 149.99},
+  ];
+}
