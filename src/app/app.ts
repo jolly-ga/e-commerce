02c75b5676e81ef4f02ed442985e7361cf6c088f
Ana Garcia <jolly20222368@gmail.com>
2026-07-17 19:15:10 -0300

    docs(app.ts): muda o caminho de imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';//remove a importação do RouterOutlet, pois não é necessário para esse componente
-import{Produto} from './components/produto/produto'; //importando a classe produto do arquivo produto.ts para ser usado no componente app
+//import{Produto} from './components/produto/produto'; //importando a classe produto do arquivo produto.ts para ser usado no componente app
+import{ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
