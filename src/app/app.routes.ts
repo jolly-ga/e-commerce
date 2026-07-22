a8162c493d67954d8eef8209ad5634a472765e7a
Ana Garcia <jolly20222368@gmail.com>
2026-07-22 19:02:35 -0300

    refactor: routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import{ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import{Home} from './features/home/home/home'
+export const routes: Routes = [
+    {
+        path:'',
+        component: Home,
+    },
+    {
+        path:'produtos',
+        component: ListaProdutos
+    },
+     {
+        path:'carrinho',
+        component: Carrinho
+    },
+    {
+        path:'**',
+        redirectTo:'',
+    }
+];
