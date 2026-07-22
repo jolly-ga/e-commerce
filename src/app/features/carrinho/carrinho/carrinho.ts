8236954337134f076caf3a080c58d6509d570de7
Ana Garcia <jolly20222368@gmail.com>
2026-07-22 19:10:46 -0300

    feat(navegação) cria tela carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {}
+//! lógica de carrinho virá nas próximas aulas
+//! por hora; Apenas exibe a página carrinho
