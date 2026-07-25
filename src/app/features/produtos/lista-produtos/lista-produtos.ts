633d539c86fe5514dc9f860041bbc0a70bacc5ac
Ana Garcia <jolly20222368@gmail.com>
2026-07-24 21:27:15 -0300

    feat(features): cria metodo de navegação

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -5,6 +5,10 @@ import{computed} from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import{effect} from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { produtosService } from '../produtos.service';
+import { inject } from '@angular/core'; 
+
+
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
@@ -12,26 +16,33 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //lista de dados-Array
-  produtos = signal( [
-    {nome:'Teclado Gamer', preco:229.99},
-    {nome:'Mouse Gamer', preco: 129.99},
-    {nome:'Monitor Gamer', preco: 2000},
-    {nome:'Desktop Gamer', preco: 4999.99},
-    {nome:'Headset Gamer', preco: 500}
-  ]);
-   //!função para exibir produtos selecionados
+ //! SIGNALS 
+produtos = signal<{nome: string; preco: number}[]>([]);
+  carregando = signal(true);
+  
+  //! método para criar um estado de seleção com signal string | null
+  produtoSelecionado = signal <string | null>(null);
+  //! metodo para criar um estado para carrinho com signal
+  carrinho = signal <{nome: string; preco: number}[]>([]);
+
+  adicionarAoCarrinho(produto:{nome: string; preco: number}){
+    this.carrinho.update(listaAtual => [...listaAtual, produto]
+    );
+  }
+ //!função para exibir produtos selecionados
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
     this.produtoSelecionado.set(nome);
   }
-  //!função que adiciona produtos usando método update
-  adicionarProduto(){
+
+adicionarProduto(){
     this.produtos.update(listaAtual => [
       ...listaAtual,
       {nome:'Playstation 5', preco:3000},
     ]);
   }
+//!função que adiciona produtos usando método update
+  
   //!função que contabiliza a quantidade de itens na lista
   totalProdutos=computed(() => this.produtos().length);
   //função que calcula o valor total usando o método computed()
@@ -40,8 +51,7 @@ export class ListaProdutos {
     total + item.preco,0
   )}
   );
-  //!função para substitui a lista atual usando método set()
-  substituirProdutos(){
+substituirProdutos(){
     this.produtos.set([
       {nome:'Teclado', preco:50},
        {nome:'Mouse', preco:15},
@@ -50,8 +60,35 @@ export class ListaProdutos {
           {nome:'Headset', preco:30},
     ]);
   }
+  
+//!função para substitui a lista atual usando método set()
+  
+   //**inject */
+private produtosService = inject(produtosService);
+
+
+//===================metodo http clint (API)==================
+carregarProdutos(){
+this.carregando.set(true);
+this.produtosService.buscarProdutos().subscribe({
+  next: (dados) => {
+    const produtos = this.produtosService.transformarProdutos(dados);
+    this.produtos.set(produtos);
+    this.carregando.set(false);
+  },
+  error: (erro) => {
+    console.error('Erro ao carregar produtos: ', erro);
+    this.carregando.set(false);
+  }
+});
+}
+
   //! método para monitorar alterações em tempo real usando effect()
   constructor(){
+
+
+    //! Carrega a API
+    this.carregarProdutos();
     effect(()=>{
       console.log('Lista de Produtos Alterados: ', this.produtos());
     });
@@ -64,19 +101,24 @@ export class ListaProdutos {
       }
     });
   }
-  //! método para criar um estado de seleção com signal string | null
-  produtoSelecionado = signal <string | null>(null);
-  //! metodo para criar um estado para carrinho com signal
-  carrinho = signal <{nome: string; preco: number}[]>([]);
-  adicionarAoCarrinho(produto:{nome: string; preco: number}){
-    this.carrinho.update(listaAtual => [...listaAtual, produto]
-    );
-  }
   //metodo para calcular quantidade total de itens no carrinho
   quantidadeCarrinho = computed (() => this.carrinho().length);
   //metodo para calcular o valor total de itens no carrinho
   totalCarrinho = computed (() =>{
     return this.carrinho().reduce((total, item) =>
     total + item.preco,0)});
-  }
 
+}
+ 
+
+  
+  
+  
+
+
+
+
+  
+
+
+ 
