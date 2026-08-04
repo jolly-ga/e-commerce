86ffb529a0fbb52b430e7f2f036f28a0411691f2
Ana Garcia <jolly20222368@gmail.com>
2026-08-03 21:09:35 -0300

    feat(app.config.ts): faz imports para funcionalidade de interceptor e erros

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withFetch()),
-  ]
+    provideHttpClient(withFetch(),
+    withInterceptors([httpInterceptor])),
+    
+  ],
 };
