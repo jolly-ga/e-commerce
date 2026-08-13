b9f969b56c53855f9fa12f8de61187842b7d3c1d
Ana Garcia <jolly20222368@gmail.com>
2026-08-12 21:28:28 -0300

    perf: melhora login e logout

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, signal, computed } from "@angular/core";
+type Usuario ={
+    email: string;
+    perfil: string;
+
+}
+
+@Injectable({
+providedIn: 'root'
+
+})
+export class AuthService {
+    private usuario = signal<Usuario | null>(null);
+    private tokenJwt = signal<string | null>(null);
+
+    usuarioAtual = computed(() => this.usuario());
+usuarioLogado = computed(()=> this.usuario() ! == null);
+token = computed(()=> this.tokenJwt());
+
+
+login(){}
+logout(){
+    this.usuario.set(null);
+    this.tokenJwt.set(null);
+}
+obterToken(): string | null {
+    return this.tokenJwt();
+}
+}
+
