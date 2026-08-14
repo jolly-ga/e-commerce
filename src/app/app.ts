1e9ad66a33c3abd336b7aa0ff76db20a47a85792
Ana Garcia <jolly20222368@gmail.com>
2026-08-14 19:04:57 -0300

    refactor(app.html, app.ts): remove código temporário que foi substituído pela arquitetura profissional de autenticação.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,20 +3,16 @@ import { Component, signal } from '@angular/core';
 //import{Produto} from './components/produto/produto'; //importando a classe produto do arquivo produto.ts para ser usado no componente app
 import{ RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe} from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
 import {MatButtonModule } from '@angular/material/button';
 import {MatCardModule } from '@angular/material/card';
 import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatCardModule, MatButtonModule, Header],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'Mercado Liso';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
